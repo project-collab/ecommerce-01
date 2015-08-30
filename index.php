5f07161dbdaf56d52fb3391e781314dfc95989b2
Designers-Bureau <darrenbernardno1@hotmail.com>
2015-08-30 17:16:02 +0100

    E-Commerce First Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grunt-01</title>
+    <link rel="stylesheet" href="_/css/styles.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<h1>E-Commerce Tariq</h1>
+	<h2>Tariq</h2>
+
+
+<script src="_/js/script.js"></script>
+<script src="http://localhost:35729/livereload.js"></script>	
+</body>
+</html>
